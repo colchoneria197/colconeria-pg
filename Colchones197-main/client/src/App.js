286c9297c7924bda7b47df3e52ca6f0008ee64d9
alejandro <valentina.morales.paratore@gmail.com>
2022-11-27 00:19:12 -0300

    inicio de front

--- Colchones197-main/client/src/App.js
+++ Colchones197-main/client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import Navbar from './components/navbar/navbar.jsx'
+import Home from './Components/home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Colchones 197</h1>
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <React.Fragment>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            {/* <Route path="*" component={NotFound} /> */}
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     </div>
   );
 }
